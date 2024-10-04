7d6d19b03084a48e6ea104a7f13afd354b2ce55d
jangdm37 <jangdm37@gmail.com>
2024-10-04 15:00:14 +0900

    first

---
+++ src/test/java/com/chatting/chatting_test/ChattingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatting.chatting_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChattingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
